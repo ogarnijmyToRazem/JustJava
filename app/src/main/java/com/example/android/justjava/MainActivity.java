c4df84a3026ffcec939a5dd52636d6c0c4f21fdf
Michal Smigiel <michal.smigiel@onet.pl>
2017-11-29 16:49:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.android.justjava;
+
+/**
+ * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ * <p>
+ * package com.example.android.justjava;
+ */
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int price = calculatePrice();
+        String priceMessage = createOrderSummary(price);
+        displayMessage(priceMessage);
+    }
+
+    /**
+     * Calculates the price of the order.
+     * <p>
+     * /* @param quantity is the number of cups of coffee ordered
+     * /* @param priceForOneCup the price of 1 cup
+     */
+    private int calculatePrice() {
+        return quantity * 5;
+    }
+
+    /**
+     * @param price price of an order
+     * @return message for order
+     */
+    private String createOrderSummary(int price) {
+        String message = "Name: Kaptain Kunal" +
+                "\nQuantity: " + quantity +
+                "\nTotal: $" + price +
+                "\nThank you!";
+        return message;
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        quantity = quantity + 1;
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int integer) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + integer);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+}
